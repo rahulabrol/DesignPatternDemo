fe1c13dc01e1652a739dd07b197004cfae870ea8
Rahul Abrol <rahul.abrol@rocketflyer.in>
2019-08-08 13:14:16 +0530

    bridge pattern completed

---
+++ app/src/main/java/com/example/designpatterndemo/design_patterns/structural_pattern/bridge_pattern/Sword.java
@@ -0,0 +1,32 @@
+package com.example.designpatterndemo.design_patterns.structural_pattern.bridge_pattern;
+
+public class Sword implements Weapon {
+    private Enchantment enchantment;
+
+    public Sword(Enchantment enchantment) {
+        this.enchantment = enchantment;
+    }
+
+    @Override
+    public void wield() {
+        System.out.println("The sword is wielded");
+        enchantment.onActivate();
+    }
+
+    @Override
+    public void swing() {
+        System.out.println("The sword is swing");
+        enchantment.apply();
+    }
+
+    @Override
+    public void unWield() {
+        System.out.println("The sword is unwielded");
+        enchantment.onDeactivate();
+    }
+
+    @Override
+    public Enchantment getEnchantment() {
+        return enchantment;
+    }
+}
---
+++ app/src/main/java/com/example/designpatterndemo/design_patterns/structural_pattern/bridge_pattern/Weapon.java
@@ -0,0 +1,15 @@
+package com.example.designpatterndemo.design_patterns.structural_pattern.bridge_pattern;
+
+/**
+ * Weapon
+ */
+public interface Weapon {
+
+    void wield();
+
+    void swing();
+
+    void unWield();
+
+    Enchantment getEnchantment();
+}
