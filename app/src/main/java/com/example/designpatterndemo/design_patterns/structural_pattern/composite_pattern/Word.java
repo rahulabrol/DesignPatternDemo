0137a4d4cb46116b0d6ae5ea0411780ca874f3a4
Rahul Abrol <rahul.abrol@rocketflyer.in>
2019-08-09 17:46:40 +0530

    composit design pattern completes

---
+++ app/src/main/java/com/example/designpatterndemo/design_patterns/structural_pattern/composite_pattern/Word.java
@@ -0,0 +1,17 @@
+package com.example.designpatterndemo.design_patterns.structural_pattern.composite_pattern;
+
+import java.util.List;
+
+public class Word extends LetterComposite {
+
+    public Word(List<Letter> letters) {
+        for (Letter l : letters) {
+            this.add(l);
+        }
+    }
+
+    @Override
+    protected void printThisBefore() {
+        System.out.print(" ");
+    }
+}
