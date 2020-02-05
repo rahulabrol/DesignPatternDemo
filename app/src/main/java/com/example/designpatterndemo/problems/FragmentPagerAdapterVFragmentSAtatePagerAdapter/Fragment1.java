b09f33c27d78d420c775b33932eac3308721e88e
Rahul Abrol <rahul.abrol@rocketflyer.in>
2020-02-05 17:18:15 +0530

    new methods are added

--- app/src/main/java/com/example/designpatterndemo/problems/FragmentPagerAdapterVFragmentSAtatePagerAdapter/Fragment1.java
+++ app/src/main/java/com/example/designpatterndemo/problems/FragmentPagerAdapterVFragmentSAtatePagerAdapter/Fragment1.java
@@ -33,6 +33,30 @@ public class Fragment1 extends Fragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop");
+    }
+
+    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         Log.e(TAG, "onActivityCreated");
