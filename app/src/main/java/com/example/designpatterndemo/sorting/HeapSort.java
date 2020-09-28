6493287d528e1f3d3a8b1b6d9819f533c04f089f
rahulabrol2211 <rahulabrol2211@gmail.com>
2020-09-28 20:24:56 +0530

    some modifications

--- app/src/main/java/com/example/designpatterndemo/sorting/HeapSort.java
+++ app/src/main/java/com/example/designpatterndemo/sorting/HeapSort.java
@@ -31,7 +31,6 @@ public class HeapSort {
     // Driver program
     public static void main(String[] args) {
         int[] arr = {12, 11, 13, 5, 6, 7};
-        int n = arr.length;
 
         HeapSort ob = new HeapSort();
         ob.sort(arr);
