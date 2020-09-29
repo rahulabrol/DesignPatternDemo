f1a3452bbd5806510b23c553d838b9969709fd8f
rahulabrol2211 <rahulabrol2211@gmail.com>
2020-09-29 20:35:23 +0530

    coments added for test

--- app/src/test/java/com/example/designpatterndemo/ExampleUnitTest.kt
+++ app/src/test/java/com/example/designpatterndemo/ExampleUnitTest.kt
@@ -4,14 +4,12 @@ import com.example.designpatterndemo.sorting.QuickSort
 import org.junit.Test
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * To run main method use this class for checking
+ * the functionality of the class
  */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         QuickSort.main(null)
-//        Assignment.main(null)
     }
 }
