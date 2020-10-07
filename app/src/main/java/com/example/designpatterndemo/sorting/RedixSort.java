3acfe042ffa2af92d59e70912667b384d01f79bf
rahulabrol2211 <rahulabrol2211@gmail.com>
2020-10-07 22:40:22 +0530

    redix sort description added

--- app/src/main/java/com/example/designpatterndemo/sorting/RedixSort.java
+++ app/src/main/java/com/example/designpatterndemo/sorting/RedixSort.java
@@ -1,5 +1,15 @@
 package com.example.designpatterndemo.sorting;
 
+/**
+ * In computer science, radix sort is a non-comparative sorting algorithm. It avoids
+ * comparison by creating and distributing elements into buckets according to their radix. For
+ * elements with more than one significant digit, this bucketing process is repeated for each
+ * digit, while preserving the ordering of the prior step, until all digits have been considered.
+ * For this reason, radix sort has also been called bucket sort and digital sort.
+ *
+ * Radix sort can be applied to data that can be sorted lexicographically, be they integers, words,
+ * punch cards, playing cards, or the mail.
+ */
 public class RedixSort {
     public static void main(String[] args) {
 
